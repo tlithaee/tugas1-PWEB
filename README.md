62ee0cbded1481fe7a3e0747bc23753ef8e4dc7f
tlithaee <syarifah.talithae@gmail.com>
2023-09-08 17:32:54 +0700

    modified:   README.md 	renamed:    index.css -> assets/index.css 	new file:   assets/index.js 	modified:   index.html

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# 
+berikut adalah tugas untuk PWEB
+
+. . . (otw nnti nyusul ygy)
---
+++ assets/index.js
@@ -0,0 +1,29 @@
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+    
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    } )
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== ADD BLUR TO HEADER ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
